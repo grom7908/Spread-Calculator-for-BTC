fcf7484b38f62648c3fab289ea24c17862a6eb04
grom7908 <vladyslav.pp@gmail.com>
2023-12-22 18:36:48 +0200

    version 1.2

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def get_crypto_price(url, accept_locator, price_locator):
         driver.get(url)
         time.sleep(2)
 
-        find_accept = driver.find_element(*accept_locator)
-        find_accept.click()
+        if accept_locator:
+            find_accept = driver.find_element(*accept_locator)
+            find_accept.click()
 
         find_price = driver.find_element(*price_locator)
         crypto_price = find_price.get_attribute('innerHTML')
@@ -30,4 +31,14 @@ accept_locator_binance = (By.ID, "onetrust-accept-btn-handler")
 price_locator_binance = (
     By.XPATH, "/html/body/div[3]/div/main/div[1]/div/div/div[2]/div[1]/div/div/a[1]/div[2]/div/div")
 
+url_bybit = "https://www.bybit.com/en"
+price_locator_bybit = (
+    By.CSS_SELECTOR, ".SectionList_price__3cLEn.SectionList_right__1nar7")
+
+url_okx = "https://www.okx.com"
+accept_locator_okx = (By.ID, "onetrust-accept-btn-handler")
+price_locator_okx = (By.CSS_SELECTOR, "a.ticker-item")
+
 get_crypto_price(url_binance, accept_locator_binance, price_locator_binance)
+get_crypto_price(url_bybit, None, price_locator_bybit)
+get_crypto_price(url_okx, accept_locator_okx, price_locator_okx)
