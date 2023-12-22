f1bf607122ac93bbd4e44e6889f0307a4685b9f3
grom7908 <vladyslav.pp@gmail.com>
2023-12-22 17:43:19 +0200

    1.1 version

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-service = Service()
-option = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=option)
 
-url = "https://www.binance.com/en"
-driver.get(url)
-time.sleep(2)
+def get_crypto_price(url, accept_locator, price_locator):
+    driver = webdriver.Chrome()
 
-find_accept1 = driver.find_element(By.ID, "onetrust-accept-btn-handler")
-find_accept1.click()
+    try:
+        driver.get(url)
+        time.sleep(2)
 
-find_btc_1 = driver.find_element(
+        find_accept = driver.find_element(*accept_locator)
+        find_accept.click()
+
+        find_price = driver.find_element(*price_locator)
+        crypto_price = find_price.get_attribute('innerHTML')
+
+        print(f"Cryptocurrency price is {crypto_price}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        driver.quit()
+
+
+url_binance = "https://www.binance.com/en"
+accept_locator_binance = (By.ID, "onetrust-accept-btn-handler")
+price_locator_binance = (
     By.XPATH, "/html/body/div[3]/div/main/div[1]/div/div/div[2]/div[1]/div/div/a[1]/div[2]/div/div")
-find_btc_1 = find_btc_1.get_attribute('innerHTML')
-print("BTC current price is " + find_btc_1)
+
+get_crypto_price(url_binance, accept_locator_binance, price_locator_binance)
